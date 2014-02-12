093b835a20958d67349a8506f217625a9574788e
Shaun <discospider@me.com>
2014-02-12 15:17:34 -0500

    basic based on tactical
    such craziness

--- _2014New/_2014New.ino
+++ _2014New/_2014New.ino
@@ -4,10 +4,24 @@
  BETA!
  
  */
-#include <hexbright.h>
-#include <Wire.h>
+#include <print_power.h>
 
-hexbright hb();
+#define BUILD_HACK
+#include <hexbright.h>
+
+
+#define MS 5
+hexbright hb;
+
+#define HOLD_TIME 250 // milliseconds before going to strobe
+#define OFF_TIME 650 // milliseconds before going off on the next normal button press
+
+#define BRIGHTNESS_COUNT 4
+#define BRIGHTNESS_OFF BRIGHTNESS_COUNT-1
+int brightness[BRIGHTNESS_COUNT] = {1000, 600, 300, OFF_LEVEL};
+int current_brightness = BRIGHTNESS_OFF; // start on the last mode (off)
+
+unsigned long short_press_time = 0;
 
 void setup() {
   hb.init_hardware(); 
@@ -16,15 +30,33 @@ void setup() {
 void loop() {
   hb.update();
   
-  # turn on
-  if(hb.button_released() && hb.button_held() < 300/MS) {
-    hb.set_light(CURRENTLEVEL, MAX_LOW_LEVEL, 120/MS)
-  }
-  
-  # turn off
-  if(hb.button_held() > 300/MS) {
-    hb.shutdown();
-  }
+  if(hb.button_just_released()) {
+    
+    if(hb.button_pressed_time()<HOLD_TIME) {
+      // we just had a normal duration press
+      if(short_press_time+OFF_TIME<millis() && current_brightness!=BRIGHTNESS_OFF) {
+        // it's been a while since our last button press, turn off
+        current_brightness = BRIGHTNESS_OFF;
+      } else {
+        short_press_time = millis();
+        current_brightness = (current_brightness+1)%BRIGHTNESS_COUNT;
+      }
+    } else {
+      // we have been doing strobe, set light at the previous light level (do nothing to current_brightness)
+    }
+    // actually change the brightness
+    hb.set_light(CURRENT_LEVEL, brightness[current_brightness], 50);
+  } else if (hb.button_pressed() && hb.button_pressed_time()>HOLD_TIME) {
+    // held for over HOLD_TIME ms, go to strobe
+    static unsigned long flash_time = millis();
+    if(flash_time+70<millis()) { // flash every 70 milliseconds
+      flash_time = millis(); // reset flash_time
+      hb.set_light(MAX_LEVEL, 0, 20); // and pulse (going from max to min over 20 milliseconds)
+      // actually, because of the refresh rate, it's more like 'go from max brightness on high
+      //  to max brightness on low to off.
+    }
+  } 
+  print_power();
   
   
 }
