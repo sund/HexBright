67f5111f3ddd4e7e7f287979c0e5b04c7b9ae830
Shaun <discospider@me.com>
2014-10-22 21:35:38 -0400

    Update with a skeleton to work from

--- SJSHexBrightFLEX/SJSHexBrightFLEX.ino
+++ SJSHexBrightFLEX/SJSHexBrightFLEX.ino
@@ -1,344 +1,72 @@
-///*
-// My version of HexBrightFLEX based on hexbright_bjh
-// vDEV (change the 'Powered Up! text below to match)
+//
+// My version of HexBrightFLEX based on hexbright
 // __________________________________________________
 //
-// See sund/HexBright/HexBright_Mine/SJSHexBrightFLEX/
-// README.md
+// See README.md
 // __________________________________________________
 //
 // __________________________________________________
-// See the sund/HexBright/README.md for info on sources.
+// See the README.md for info on sources.
 //
-// */
+//
 
-//#include <Wire.h>
-//#include <pc_stubs.h>
-//#include <read_adc.h>
-//#include <set_light_level.h>
-//#include <twi.h>
-//#include <update_spin.h>
-
-//#include <Time.h>
-//#include <digitalWriteFast.h>
-//
-//#include <click_counter.h>
-//#include <input_digit.h>
-//#include <print_binary.h>
-//#include <print_number.h>
 #include <print_power.h>
-//#include <strobe.h>
-//needed with hexbright.h
-hexbright hb;
+#include <print_number.h>
 
 // These next two lines must come after all other library #includes
 #define BUILD_HACK
 #include <hexbright.h>
 
+hexbright hb;
+
 // Settings
-#define OVERTEMP                300
+#define OVERTEMP 300 // When over this, we turn off or drop light level
+#define BUTTONMINPRESS 300 // min time for a button press to matter
+#define BUTTONMAXPRESS 650 // max time for a button press to matter
+#define DAZZLETIME 1000 // max time for a button press to matter and go to dazzle
 
-// Pin assignments not declared in hexbright.h
-#define DPIN_RLED_SW            2
-#define DPIN_PWR                8
-#define DPIN_DRV_MODE           9
-#define DPIN_DRV_EN             10
 
-// Modes
-#define MODE_OFF                0
-#define MODE_LOW                1
-#define MODE_MED                2
-#define MODE_HIGH               3
-#define MODE_DAZZLE             4
-#define MODE_DAZZLE_PREVIEW     5
-#define MODE_SOS                6
-#define MODE_SOS_PREVIEW        7
-
-// Variables
-const unsigned int SerialPrintIntrvl = 1000;
-const unsigned long ActTimeOut = 600;
-
-// State; initialize
-byte mode = 0;
-unsigned long btnTime = 0;
-boolean btnDown = false;
-boolean dazzle_on = true;
-long dazzle_period = 100;
 
 void setup()
 {
   //needed with hexbright.h
   hb.init_hardware();
-  // We just powered on!  That means either we got plugged
-  // into USB, or the user is pressing the power button.
-
-  // Initialize serial busses
-  Serial.begin(9600);
-
-  // Configure accelerometer
-  btnTime = millis();
-  btnDown = hb.button_pressed();
-  mode = MODE_OFF;
-
-  Serial.println("Powered up! vDEV");
-  randomSeed(analogRead(1));
 }
 
 void loop()
 {
-  //needed with hexbright.h
   hb.update();
-
-  static unsigned long lastDazzleTime, lastTempTime, lastChrgTime, lastModeTime, lastAccTime, lastModeSOSTime;
-  static unsigned long ditdah;
-  static int ledState = LOW;
-
-  unsigned long time = millis(); // time on in millsecs
-
-  // Check the state of the charge controller
-  int chargeState = hb.get_charge_state();
-
-  // Print the charge every so often
-  if (time - lastChrgTime > SerialPrintIntrvl)
-  {
-    lastChrgTime = time;
-    if (chargeState = 3)
-      Serial.println("Charged!");
-    else if (chargeState = 1)
-      Serial.println("Charging!");
-    else
-      Serial.println("On Battery!");
-  }
-
-  //  printing charge state
-  //hb.print_charge(GLED);
-
-  // Check the temperature sensor
-  if (time - lastTempTime > SerialPrintIntrvl)
-  {
-    lastTempTime = time;
-    int temperature = hb.get_thermal_sensor();
-    Serial.print("Temp: ");
-    Serial.println(temperature);
-    if (temperature > OVERTEMP && mode != MODE_OFF)
-    {
-      Serial.println("** Overheating!");
-
-      for (int i = 0; i < 6; i++)
-      {
-        hb.set_light(MAX_LOW_LEVEL, MAX_LOW_LEVEL, 100);
-        hb.set_light(MAX_LEVEL, MAX_LEVEL, 100);
-      }
-      hb.set_light(MAX_LOW_LEVEL, MAX_LOW_LEVEL, 0);
-
-      mode = MODE_LOW;
+  static int brightness_level = 4;
+  
+  // get current state
+  int state = hb.get_light_level();
+  
+  // list of power levels {1st, 2nd, 3rd, 4th}
+  int levels[] = {250,500,750,1000};
+  
+  // Pressing buttons
+  if(hb.button_just_released()) {
+  // if we are off and the button is pressed for < BUTTONMINPRESS ... power on to 1st level
+    if(state=0 && hb.button_pressed_time()<BUTTONMINPRESS) {
+      brightness_level = (brightness_level+1)%5;
+      hb.set_light(CURRENT_LEVEL, levels[brightness_level], 125);
+    
+   // else if being pressed for > DAZZLETIME... go to dazzle
+     
+    
+   // if in dazzle and button is pressed < BUTTONMINPRESS... turn off
+   
+     // else if in dazzle and button held for > BUTTONMAXPRESS then go to 1st level
+   
+   // if we are on any powerlevel and a button is pressed for > BUTTONMINPRESS and < BUTTONMAXPRESS... turn off
+   
+     // else go to next level
+     
+   // if we are in any power level and button pressed > BUTTONMAXPRESS... go to dazzle
+  
     }
   }
-
-  // Check if the accel has anything to say
-  // at a resonable interval
-//  byte tapped = 0, shaked = 0;
-//  if (!digitalRead(DPIN_ACC_INT))
-//  {
-//    Wire.beginTransmission(ACC_ADDRESS);
-//    Wire.write(ACC_REG_TILT);
-//    Wire.endTransmission(false);       // End, but do not stop!
-//    Wire.requestFrom(ACC_ADDRESS, 1);  // This one stops.
-//    byte tilt = Wire.read();
-//
-//    if (time - lastAccTime > 50)
-//    {
-//      lastAccTime = time;
-//
-//      if (hb.tapped()) Serial.println("** Tap!");
-//      if (hb.shaked()) Serial.println("** Shake!");
-//    }
-//  }
-
-  // If we are in the special modes (DAZZLE or SOS)
-  // then flip the switch on and off as needed.
-  switch (mode)
-  {
-    case MODE_DAZZLE:
-    case MODE_DAZZLE_PREVIEW:
-      if (time - lastDazzleTime > dazzle_period)
-      {
-        digitalWrite(DPIN_DRV_EN, dazzle_on);
-        dazzle_on = !dazzle_on;
-        lastDazzleTime = time;
-        dazzle_period = random(25, 100);
-      }
-      break;
-    case MODE_SOS:
-    case MODE_SOS_PREVIEW:
-      digitalWrite(DPIN_DRV_EN, morseCodeSOS(time - lastModeSOSTime));
-      break;
-  }
-
-  // Periodically pull down the button's pin, since
-  // in certain hardware revisions it can float.
-  // Do we have to do this on the shipping version of hexbright?
-  pinMode(DPIN_RLED_SW, OUTPUT);
-  pinMode(DPIN_RLED_SW, INPUT);
-
-  // Check for mode changes
-  byte newMode = mode;
-  byte newBtnDown = hb.button_pressed();
-  switch (mode)
-  {
-    case MODE_OFF:
-      if (btnDown && !newBtnDown && (time - btnTime) > 20)
-        newMode = MODE_LOW;
-      if (btnDown && newBtnDown && (time - btnTime) > 500)
-        newMode = MODE_DAZZLE_PREVIEW;
-      break;
-    case MODE_LOW:
-      if (btnDown && !newBtnDown && (time - btnTime) > 50)
-        if (time - lastModeTime > 1000)
-          newMode = MODE_OFF;
-        else newMode = MODE_MED;
-      break;
-    case MODE_MED:
-      if (btnDown && !newBtnDown && (time - btnTime) > 50)
-        if (time - lastModeTime > 1000)
-          newMode = MODE_OFF;
-        else newMode = MODE_HIGH;
-      break;
-    case MODE_HIGH:
-      if (btnDown && !newBtnDown && (time - btnTime) > 50)
-        newMode = MODE_OFF;
-      break;
-    case MODE_DAZZLE_PREVIEW:
-      // This mode exists just to ignore this button release.
-      if (btnDown && !newBtnDown)
-        newMode = MODE_DAZZLE;
-      break;
-    case MODE_DAZZLE:
-      if (btnDown && !newBtnDown && (time - btnTime) > 50)
-        newMode = MODE_OFF;
-      if (btnDown && newBtnDown && (time - btnTime) > 2000) //full 2 second press
-        newMode = MODE_SOS_PREVIEW;
-      break;
-    case MODE_SOS_PREVIEW:
-      // This mode exists just to ignore this button release.
-      if (btnDown && !newBtnDown)
-        newMode = MODE_SOS;
-      break;
-    case MODE_SOS:
-      if (btnDown && !newBtnDown && (time - btnTime) > 50)
-        newMode = MODE_OFF;
-      break;
-  }
-
-  //activity power down EXCLUDES SOS MODE!
-  if ((time - max(lastAccTime, lastModeTime) > ActTimeOut ) && newMode != MODE_SOS) {
-    newMode = MODE_OFF;
-    Serial.println("** Inactivity shutdown!");
-  }
-
-  // Do the mode transitions
-  if (newMode != mode)
-  {
-    lastModeTime = millis();
-
-    // Enable or Disable accelerometer
-//    byte disable[] = {
-//      ACC_REG_MODE, 0x00
-//    };  // Mode: standby!
-//    byte enable[] = {
-//      ACC_REG_MODE, 0x01
-//    };  // Mode: active!
-//    //Wire.beginTransmission(ACC_ADDRESS);
-//    if (newMode == MODE_OFF) {
-//      Wire.write(disable, sizeof(disable));
-//    }
-//    else Wire.write(enable, sizeof(enable));
-//    Wire.endTransmission();
-
-    switch (newMode)
-    {
-      case MODE_OFF:
-        Serial.println("Mode = off");
-        hb.set_light(-1, -1, 0);;
-        break;
-      case MODE_LOW:
-        Serial.println("Mode = low");
-        hb.set_light(MAX_LOW_LEVEL, 250, 0);
-        break;
-      case MODE_MED:
-        Serial.println("Mode = medium");
-        hb.set_light(MAX_LOW_LEVEL, MAX_LOW_LEVEL, 0);
-        break;
-      case MODE_HIGH:
-        Serial.println("Mode = high");
-        hb.set_light(MAX_LEVEL, MAX_LEVEL, 0);
-        break;
-      case MODE_DAZZLE:
-      case MODE_DAZZLE_PREVIEW:
-        Serial.println("Mode = dazzle");
-        pinMode(DPIN_PWR, OUTPUT);
-        digitalWrite(DPIN_PWR, HIGH);
-        digitalWrite(DPIN_DRV_MODE, HIGH);
-        break;
-      case MODE_SOS:
-      case MODE_SOS_PREVIEW:
-        Serial.println("Mode = SOS");
-        pinMode(DPIN_PWR, OUTPUT);
-        digitalWrite(DPIN_PWR, HIGH);
-        digitalWrite(DPIN_DRV_MODE, HIGH);
-        lastModeSOSTime = time;
-        break;
-    }
-
-    mode = newMode;
-  }
-
-  // Remember button state so we can detect transitions
-  if (newBtnDown != btnDown)
-  {
-    btnTime = time;
-    btnDown = newBtnDown;
-    delay(50);
-  }
+  // Print power status on green LED
+  print_power();
 }
-
-bool morseCodeSOS(unsigned long time) {
-  const unsigned long dit = 180;
-  // 180 ms is the frame for each dit "on", the larger this number the slower the SOS
-
-  // Morse Code:  (thanks jaebird!)
-  // S = ...  O = ---
-  // SOS word = ...---...
-
-  // word space = 7 dits duration
-  // S = 5 dits duration
-  // char space = 3 dits duration
-  // O = 11 dits duration
-  // char space = 3 dits duration
-  // S = 5 dits duration
-  // total duration = 34
-
-  byte step = (time / dit) % 34; //dit number modulo the length of the sequence;
-  // Start with word space
-  if (step < 7) return false;
-  step -= 7;
-  // First S
-  if (step < 5) return (step % 2) == 0; // every second dit is off
-  step -= 5;
-  // Char space
-  if (step < 3) return false;
-  step -= 3;
-  // O
-  if (step < 11) return (step % 4) != 3; // every fourth dit is off
-  step -= 11;
-  // Char space
-  if (step < 3) return false;
-  step -= 3;
-  // Last S
-  if (step < 5) return (step % 2) == 0; // every second dit is off
-  // Should never get here
-  Serial.println("Morse SOS overrun error");
-  return false;
-}
-
 
