05a5eeb00afb5f4a09440841e02a40f2c593e479
Shaun <discospider@me.com>
2015-02-12 21:58:50 -0500

    Something

--- SJSHexBrightFLEX/SJSHexBrightFLEX.ino
+++ SJSHexBrightFLEX/SJSHexBrightFLEX.ino
@@ -12,61 +12,82 @@
 
 #include <print_power.h>
 #include <print_number.h>
+#include <strobe.h>
 
 // These next two lines must come after all other library #includes
 #define BUILD_HACK
 #include <hexbright.h>
-
-hexbright hb;
 
 // Settings
 #define OVERTEMP 300 // When over this, we turn off or drop light level
 #define BUTTONMINPRESS 300 // min time for a button press to matter
 #define BUTTONMAXPRESS 650 // max time for a button press to matter
 #define DAZZLETIME 1000 // max time for a button press to matter and go to dazzle
+#define CYCLETRANSITION 75 // time for light level transistion while in CYCLE_MODE
+
+static int brightness_level = 4;
+
+#define OFF_MODE 0
+#define DAZZLE_MODE 1
+#define CYCLE_MODE 2
+
+int mode = 0;
 
 
+hexbright hb;
 
-void setup()
-{
-  //needed with hexbright.h
+void setup() {
   hb.init_hardware();
 }
 
-void loop()
-{
+void loop() {
   hb.update();
-  static int brightness_level = 4;
 
-  // get current state
-  int state = hb.get_light_level();
-
-  // list of power levels {1st, 2nd, 3rd, 4th}
-  int levels[] = {250, 500, 750, 1000};
-
-  // Pressing buttons
+  //// Button actions to recognize, one-time actions to take as a result
   if (hb.button_just_released()) {
-    // if we are off and the button is pressed for < BUTTONMINPRESS ... power on to 1st level
-    if (state = 0 && hb.button_pressed_time() < BUTTONMINPRESS) {
+    if (hb.button_pressed_time() < BUTTONMINPRESS) {
+      mode = CYCLE_MODE;
+      int levels[] = {1, 250, 500, 750, 1000};
       brightness_level = (brightness_level + 1) % 5;
-      hb.set_light(CURRENT_LEVEL, levels[brightness_level], 125);
+      hb.set_light(CURRENT_LEVEL, levels[brightness_level], CYCLETRANSITION);
+      // while cycling, if held for less than dazzle time but more than BUTTONMINPRESS
+    } else if (hb.button_pressed_time() < DAZZLETIME) {
+      mode = DAZZLE_MODE;
+    }
 
-      // else if being pressed for > DAZZLETIME... go to dazzle
+  }
 
-
-      // if in dazzle and button is pressed < BUTTONMINPRESS... turn off
-
-      // else if in dazzle and button held for > BUTTONMAXPRESS then go to 1st level
-
-      // if we are on any powerlevel and a button is pressed for > BUTTONMINPRESS and < BUTTONMAXPRESS... turn off
-
-      // else go to next level
-
-      // if we are in any power level and button pressed > BUTTONMAXPRESS... go to dazzle
-
+  // strobe if off and held for > DAZZLETIME
+  if (hb.button_pressed()) {
+    if (hb.button_pressed_time() > DAZZLETIME && mode == OFF_MODE) {
+      mode = DAZZLE_MODE;
     }
   }
+
+  // turn off
+  if (hb.button_pressed_time() > BUTTONMAXPRESS ) {
+    mode = OFF_MODE;
+    hb.set_light(CURRENT_LEVEL, OFF_LEVEL, NOW);
+    // in case we are under usb power, reset state
+    brightness_level = 4;
+  }
+
+  //// Actions over time for a given mode
+  if (mode == DAZZLE_MODE) { // just
+  //set_strobe_fpm(1000);
+        static int i = 0;
+        if (!i) {
+          // fade from max to 0 over a random time btwn 30 and 350 milliseconds (length flash "on")
+          hb.set_light(MAX_LEVEL, 0, random(30, 250));
+          // only light up every random number of times btwn 6 and 60 through
+          // which should equate to 50 - 500 ms (length flash "off")
+          i = random(50, 500) / 8.3333;
+        }
+        i--;
+  }
+
   // Print power status on green LED
   print_power();
 }
+
 
